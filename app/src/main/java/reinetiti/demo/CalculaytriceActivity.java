c73c2693dc89f44d046b2565e7b014783dd6b066
Reinetiti <jeanneanti01@gmail.com>
2021-11-25 11:59:37 +0100

    création de l'activité

---
+++ app/src/main/java/reinetiti/demo/CalculaytriceActivity.java
@@ -0,0 +1,14 @@
+package reinetiti.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculaytriceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculaytrice);
+    }
+}
